a5eda7d0fbb2af87397d9bfbe3cc82548787bedc
Miguel Fernando Urquijo Gómez <miguelferu@gmail.com>
2019-06-05 15:31:36 -0500

    Creación de templates

--- src/js/home.js
+++ src/js/home.js
@@ -7,21 +7,21 @@ function cambiarNombre(nuevoNombre) {
     cambia = nuevoNombre
 }
 
-// const getUserAll = new Promise(function(todoBien, todoMal) {
-//     // llamar a un api
-//     setTimeout(function() {
-//         // luego de 3 segundos
-//         todoBien('se acabó el tiempo');
-//     }, 5000)
-// })
+const getUserAll = new Promise(function(todoBien, todoMal) {
+    // llamar a un api
+    setTimeout(function() {
+        // luego de 3 segundos
+        todoBien('se acabó el tiempo');
+    }, 5000)
+})
 
-// const getUser = new Promise(function(todoBien, todoMal) {
-//     // llamar a un api
-//     setTimeout(function() {
-//         // luego de 3 segundos
-//         todoBien('se acabó el tiempo 3');
-//     }, 3000)
-// })
+const getUser = new Promise(function(todoBien, todoMal) {
+    // llamar a un api
+    setTimeout(function() {
+        // luego de 3 segundos
+        todoBien('se acabó el tiempo 3');
+    }, 3000)
+})
 
 // getUser
 //   .then(function() {
@@ -31,40 +31,40 @@ function cambiarNombre(nuevoNombre) {
 //     console.log(message)
 //   })
 
-// Promise.race([
-//         getUser,
-//         getUserAll,
-//     ])
-//     .then(function(message) {
-//         console.log(message);
-//     })
-//     .catch(function(message) {
-//         console.log(message)
-//     })
+Promise.race([
+        getUser,
+        getUserAll,
+    ])
+    .then(function(message) {
+        console.log(message);
+    })
+    .catch(function(message) {
+        console.log(message)
+    })
 
 
 
-// $.ajax('https://randomuser.me/api/sdfdsfdsfs', {
-//     method: 'GET',
-//     success: function(data) {
-//         console.log(data)
-//     },
-//     error: function(error) {
-//         console.log(error)
-//     }
-// })
+$.ajax('https://randomuser.me/api/sdfdsfdsfs', {
+    method: 'GET',
+    success: function(data) {
+        console.log(data)
+    },
+    error: function(error) {
+        console.log(error)
+    }
+})
 
-// fetch('https://randomuser.me/api/dsfdsfsd')
-//     .then(function(response) {
-//         // console.log(response)
-//         return response.json()
-//     })
-//     .then(function(user) {
-//         console.log('user', user.results[0].name.first)
-//     })
-//     .catch(function() {
-//         console.log('algo falló')
-//     });
+fetch('https://randomuser.me/api/dsfdsfsd')
+    .then(function(response) {
+        // console.log(response)
+        return response.json()
+    })
+    .then(function(user) {
+        console.log('user', user.results[0].name.first)
+    })
+    .catch(function() {
+        console.log('algo falló')
+    });
 
 (async function load() {
     async function getData(url) {
@@ -73,16 +73,47 @@ function cambiarNombre(nuevoNombre) {
         return data;
 
     }
+//debugger
+const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
+  const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama')
+  const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation')
+  console.log(actionList, dramaList, animationList)
 
-    const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
-    const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama')
-    const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation')
-        // getData('https: //yts.am/api/v2/list_movies.json?genre=drama')
-        //     .then(function(data) {
-        //         console.log('dramaList', data);
-        //         dramaList = data;
-        //     })
-    console.log('actionList', actionList);
-    console.log('dramaList', dramaList);
-    console.log('animationList', animationList);
+  function videoItemTemplate (movie) {
+    return (
+        `<div class="primaryPlaylistItem">
+        <div class="primaryPlaylistItem-image">
+          <img src="${movie.medium_cover_image}">
+        </div>
+        <h4 class="primaryPlaylistItem-title">
+          ${movie.title}
+        </h4>
+      </div>`
+    )
+}
+
+// console.log(videoItemTemplate(movie));
+
+    actionList.data.movies.forEach((movie) => {
+        //debugger
+        const HTMLString = videoItemTemplate(movie);
+        console.log(HTMLString);
+    });
+    
+    const $actionContainer = document.querySelector('#action');
+    const $dramaContainer = document.getElementById('drama');
+    const $animationContainer = document.getElementById('animation');
+
+    const $featuringContainer = document.getElementById('featuring');
+    const $form = document.getElementById('form');
+    const $home = document.getElementById('home');
+
+    const $modal = document.getElementById('modal');
+    const $overlay = document.getElementById('overlay');
+    const $hidemodal = document.getElementById('hide-modal');
+
+    const $modalTitle = $modal.querySelector('h1');
+    const $modalImage = $modal.querySelector('img');
+    const $modalDescription = $modal.querySelector('p');
+
 })()
